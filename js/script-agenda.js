03c7f9b1102136e3b47642346d1b976ca65f2914
Eduardo-Roots <eduardo7589985@gmail.com>
2022-03-03 16:56:00 -0300

    Ajuste nos lembretes

--- js/script-agenda.js
+++ js/script-agenda.js
@@ -1,36 +1,31 @@
 let lembretes = [];
 let output = [];
+let users = 'notes'
+
 
 function formatDate(date) {
-    // formata a data para o formato DD/MM/YYYY
     const time = new Date(date);
     return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
 }
 
 function showList() {
-    // mostra a lista de todo
     if (lembretes.length > 0) {
-        const htmlTemp = (`<ol> ${lembretes.map(itens => `<li> ${formatDate(itens.date)} --> ${itens.descricao}`)} </ol> <button class="ml-5 mt-5 bg-danger text-light">Apagar tudo do dia</button>`);
+        const htmlTemp = (`<ol> ${lembretes.map(itens => `<li> ${formatDate(itens.date)} --> ${itens.notes}`)} </ol> <button class="ml-5 mt-5 bg-danger text-light">Apagar tudo do dia</button>`);
         output.innerHTML = htmlTemp;
     } else {
-        output.innerHTML = 'Você ainda não tem lembretes!';
+        output.innerHTML = 'Você apagou todos seus lembretes!';
     }
 }
 
 function saveList() {
-    // converte os dados em string e salva no local storage
-    localStorage.setItem('tasks', JSON.stringify(lembretes));
+    localStorage.setItem(users, JSON.stringify(lembretes));
 }
 
 function clearList() {
-    // varre a lista a procura de tarefas realizadas
     for (let i = 0; i < lembretes.length; i++) {
         if (lembretes[i].done === 'false') {
-            // remove 1 elemento na posição i;
             lembretes.splice(i);
-            // localStorage.clear('tasks', JSON.stringify(lembretes))
-            localStorage.removeItem('tasks', JSON.stringify(lembretes))
-            // voltando o indice no array para validar novamente a lista
+            localStorage.removeItem(users, JSON.stringify(lembretes))
             i = 0;
         } else {
             lembretes[i].id = i;
@@ -41,7 +36,6 @@ function clearList() {
 }
 
 function clickList(element) {
-    // somente fazer algo quando clicar em um item li
     if (element.target.localName === 'li') {
         element.target.dataset.done = !element.target.dataset.done === 'true';
         lembretes[element.target.dataset.id].done = element.target.dataset.done;
@@ -51,35 +45,27 @@ function clickList(element) {
     }
 }
 
-
 function onSubmit(element) {
     const task = {};
-
-    // pego o valor cadastrado no primeiro input do meu form
-    task.descricao = element.target[0].value;
+    task.notes = element.target[0].value;
     task.date = new Date();
     task.id = lembretes.length;
     task.done = 'false';
-
-    // adicionando a task na lista
     lembretes.push(task);
     saveList();
     showList();
-    // utiliza o preventDefault para evitar do form realizar o reload da página
     element.preventDefault();
 }
 
-
-window.addEventListener('load', () => {
-    // guarda em uma variável o elemento compromisso
+window.addEventListener('load', async () => {
     output = document.getElementById('notas');
-    
-    if (localStorage.getItem('tasks')) {
-        lembretes = JSON.parse(localStorage.getItem('tasks'));
+    if (localStorage.getItem(users)) {
+        lembretes = JSON.parse(localStorage.getItem(users));
         showList();
     }
-
-    // adiciona o listener para o evento submit, utilizei form para usar o required do input HTML
+    else {
+        output.innerHTML = 'Crie seu primeiro lembrete!';
+    }
     document.getElementById('form-task').addEventListener('submit', onSubmit);
     output.addEventListener('click', clickList);
 });
