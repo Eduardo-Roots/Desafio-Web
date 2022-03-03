f368ab11f79a924f6b8f349b12eba3f161212333
Eduardo-Roots <eduardo7589985@gmail.com>
2022-03-03 14:24:42 -0300

    Correção bug sorteio

--- js/script-sorteio.js
+++ js/script-sorteio.js
@@ -1,22 +1,23 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function sorteio() {
+
     const num1 = document.getElementById("num1").value;
     const num2 = document.getElementById("num2").value;
 
-    let sort = Math.floor(Math.random() * Math.floor(num2))
-    let sort2 = Math.floor(Math.random() * Math.floor(num1))
-
     if (num2 > num1) {
-        while (sort < num1) {
-            sort = Math.floor(Math.random() * Math.floor(num2));
-        }
+        let sort = getRandomIntInclusive(num1, num2)
         document.getElementById("result").innerHTML = sort;
-    } else if(num1 > num2) {
-        while (sort2 < num2) {
-            sort2 = Math.floor(Math.random() * Math.floor(num1));
-        }
-        document.getElementById("result").innerHTML = sort2;
+
+    } else if (num1 > num2) {
+        let sort = getRandomIntInclusive(num2, num1)
+        document.getElementById("result").innerHTML = sort;
     }
-    else{
+    else {
         document.getElementById("result").innerHTML = 'Escolha um intervalo!'
     }
 }
